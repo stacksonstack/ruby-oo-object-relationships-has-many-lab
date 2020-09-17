31a419975a9be1c45e4e1913b5b7ca3de012510a
stacksonstack <staceycarrillo44@gmail.com>
2020-09-17 15:56:00 -0400

    submitted yay

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post
+    attr_accessor :title, :author
+
+    @@all = []
+
+    def initialize(title)
+        @title =title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author_name
+        if(self.author)
+            return self.author.name
+        else
+            return nil
+        end
+    end
+
+end
